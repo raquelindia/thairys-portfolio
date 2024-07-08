dd023d112cd3ecd9e10e34ced2eb8820e71f9851
Raquel Cruz <raquelicruz94@gmail.com>
2024-07-08 19:57:06 -0400

    routing

---
+++ css/script.js
@@ -0,0 +1,28 @@
+var app = angular.module('portfolioApp', ["ngRoute"]);
+app.controller('portfolioApp', function($scope) {
+
+    app.config(function($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl : "home.html",
+            controller: "appCtrl"
+        })
+        .when("/about", {
+            templateUrl : "about.html",
+            controller: "appCtrl"
+        })
+        .when("/contact", {
+            templateUrl : "contact.html",
+            controller: "appCtrl"
+        })
+        .when("/services", {
+            templateUrl : "services.html",
+            controller: "appCtrl"
+        })
+        .otherwise({
+            templateUrl : "index.html",
+            controller: "appCtrl"
+        });
+        
+    })
+});
